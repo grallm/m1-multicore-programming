5e92e5496acbcc10bad49e8f4b66f003b8d043df
ArKeid0s <matproz.gaming@gmail.com>
2022-04-08 11:55:28 +0200

    Test implementation with ExecutorService

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,16 @@ import TL2.threadpool.Task;
 import TL2.threadpool.ThreadPool;
 import tp3.Dictionary;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main
 {
+    private static volatile ExecutorService threadPool;
+
     public static void main(String[] args)
     {
-        ThreadPool threadPool = new ThreadPool(10);
-
+        threadPool = Executors.newFixedThreadPool(10);
         String[] dicWords = {"chameau", "chameaux", "chamelle", "chamelles", "chamelon", "chamelons", "chat", "chaton", "chatons", "chats", "chatte", "chattes"};
         Dictionary dic = new Dictionary();
 
@@ -18,21 +22,24 @@ public class Main
          {
              Transaction t = new TransactionTL2();
              Task task = new Task(str, t, dic);
-             threadPool.execute(task);
+             threadPool.submit(() -> {
+                 while (!t.isCommited())
+                 {
+                     try
+                     {
+                         t.begin();
+                         dic.add(str, t);
+                         t.try_to_commit();
+                     }
+                     catch (AbortException e)
+                     {
+                         e.printStackTrace();
+                     }
+                 }
+             });
          }
-        /*try {
-            for (String str : dicWords) {
-                Transaction t = new TransactionTL2();
-                while (!t.isCommited()) {
-                    t.begin();
-                    dic.add(str, t);
-                    t.try_to_commit();
-                }
-            }
-        } catch (AbortException e) {
-            e.printStackTrace();
-        }*/
 
+        System.out.println("Result :");
         System.out.println(dic.getWords());
     }
 }
